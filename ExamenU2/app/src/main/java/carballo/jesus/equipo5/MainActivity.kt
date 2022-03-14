2e50014dc6860c8e4ca3eaba843725756479437a
Almechic12 <ediithcarbherre25@gmail.com>
2022-03-14 14:30:54 -0700

    Examen

---
+++ ExamenU2/app/src/main/java/carballo/jesus/equipo5/MainActivity.kt
@@ -0,0 +1,11 @@
+package carballo.jesus.equipo5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
